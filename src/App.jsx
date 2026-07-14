4730914735333c34cc6dd506ad344f677e17e6fd
SajithNuski <sajithnuski555@gmail.com>
2026-07-15 00:53:06 +0530

    test the tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
+      <h1 class="text-3xl font-bold text-red-500 underline">
         Hello world!
       </h1>
     </>
